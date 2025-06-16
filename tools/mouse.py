70cd4b7770b1c317e8a9aecee43a2ce752f09071
LuoYiFei <122622150@qq.com>
2025-06-16 15:10:10 +0800

    暂时先移除鼠标移动的随机性，先解决鼠标偏移的问题
    暂时先移除鼠标移动的随机性，先解决鼠标偏移的问题

--- tools/mouse.py
+++ tools/mouse.py
@@ -7,22 +7,24 @@ class mouse:
      # 启用PyAutoGUI的安全特性，将鼠标移动到左上角可终止程序
     pyautogui.FAILSAFE = True
 
+    """
+        移动鼠标到指定位置
+         x: 目标X坐标
+         y: 目标Y坐标
+        duration: 移动持续时间（秒）
+    """
     @classmethod
     def move_to(cls, x, y, duration=0.5):
-        """
-        移动鼠标到指定位置
-        Args:
-            x: 目标X坐标
-            y: 目标Y坐标
-            duration: 移动持续时间（秒）
-        """
         pyautogui.moveTo(x, y, duration=duration)
 
+
+
+    """
+    点击鼠标按钮   button: 按钮类型，'left'（左键）、'right'（右键）或'middle'（中键）
+    """
     @classmethod
     def click(cls, button='left'):
-        """
-        点击鼠标按钮   button: 按钮类型，'left'（左键）、'right'（右键）或'middle'（中键）
-        """
+
         pyautogui.click(button=button)
 
 
@@ -37,22 +39,9 @@ class mouse:
     """
     @classmethod
     def move_and_click(cls, x, y, button='left'):
-
-        # 计算随机偏移量
-        import random
-        offset_range = 1  # offset_range: 随机偏移的最大范围（像素）
-        x_offset = random.randint(-offset_range, offset_range)
-        y_offset = random.randint(-offset_range, offset_range)
-
-
-        # 应用偏移量到目标坐标
-        adjusted_x = (x + x_offset)
-        adjusted_y = (y + y_offset)
-
-        print(f"目标坐标: ({x}, {y})，实际移动到: ({adjusted_x}, {adjusted_y})，偏移量: ({x_offset}, {y_offset})")
-
-        cls.move_to(adjusted_x, adjusted_y)
+        cls.move_to(x, y)
         cls.click(button)
+
 
 if __name__ == "__main__":
 
